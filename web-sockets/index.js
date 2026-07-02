1f54cecf635159b090bddeb149c314bb43c410a3
Rahmankhattak711 <rahmankhattak796@gmail.com>
2026-07-02 23:29:53 +0500

    feat: add WebSocket server with Express and Socket.IO
    - Created a new WebSocket server using Express and Socket.IO.
    - Implemented basic connection handling and message broadcasting.
    - Added package.json and pnpm-lock.yaml for dependency management.

---
+++ web-sockets/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import { createServer } from 'http'
+import { Server } from 'socket.io';
+
+const app = express();
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+  socket.on("message", (message) => {
+    io.emit("message", message);
+  });
+});
+
+httpServer.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
