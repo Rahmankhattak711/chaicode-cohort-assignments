44866e1b0e051dd1248b4254433f3a93a141e923
Rahmankhattak711 <rahmankhattak796@gmail.com>
2026-07-03 00:00:10 +0500

    feat: enhance chat interface and improve message handling in WebSocket server

--- web-sockets/index.js
+++ web-sockets/index.js
@@ -1,23 +1,24 @@
-import express from 'express';
-import { createServer } from 'http'
-import { Server } from 'socket.io';
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
 const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "*"
-  }
+    origin: "*",
+  },
 });
 
-io.on('connection', (socket) => {
-  console.log('A user connected:', socket.id);
-  socket.on("message", (message) => {
-    io.emit("message", message);
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("chat message", (message) => {
+    io.emit("message", { id: socket.id, message });
   });
 });
 
 httpServer.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
