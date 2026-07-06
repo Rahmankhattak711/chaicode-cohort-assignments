c5c7e200bc5d4d2e0781496401cc7109376d624c
Rahmankhattak711 <rahmankhattak796@gmail.com>
2026-07-06 15:17:19 +0500

    feat: update SQL queries and create tables for student and courses

--- sql-learning/05.class.sql
+++ sql-learning/05.class.sql
@@ -1,5 +1,22 @@
-select * from student
-full outer join internships on student.id = internships.student_id
+select *
+from student
+  full outer join internships on student.id = internships.student_id
+select *
+from student
+  inner join internships on student.id = internships.student_id
 
-select * from student
-inner join internships on student.id = internships.student_id
+
+
+  --  primary and foreign key
+  create table student (
+    id int primary key,
+    name varchar(100) not null,
+    email varchar(100) unique
+  )
+
+   create table courses(
+    id int primary key,
+    name varchar(100) not null,
+    student_id int,
+    foreign key (student_id) references student(id)
+  )
